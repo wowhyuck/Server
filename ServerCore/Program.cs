ab2253bd3e5a43e2abdebf0057a1c2ec549e9e75
Joohyuck <joohyuck.seo@gmail.com>
2021-06-11 16:55:30 +0900

    쓰레드 생성

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ServerCore
 {
     class Program
     {
+        static void MainThread(object state)
+        {
+            Console.WriteLine("Hellow Thread!");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+            //for (int i = 0; i < 4; i++)
+            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+
+            ThreadPool.QueueUserWorkItem(MainThread);       // Background처럼 Main이 끝나면 Thread도 종료
+
+            //Thread t = new Thread(MainThread);
+            //t.Name = "Test Thread";
+            //t.IsBackground = true;      // Main이 끝나면 해당 Thread도 종료
+            //t.Start();
+            //Console.WriteLine("Waiting for Thread");
+
+            //t.Join();
+            //Console.WriteLine("Hello World!");
+
+            while(true)
+            {
+
+            }
         }
     }
 }
