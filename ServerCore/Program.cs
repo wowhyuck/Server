8ed6647ea53c5a5fee80f083b76866063429f749
Joohyuck <joohyuck.seo@gmail.com>
2021-06-11 18:59:04 +0900

    캐시 이론

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,35 +6,26 @@ namespace ServerCore
 {
     class Program
     {
-        volatile static bool _stop = false;
-
-        static void ThreadMain()
-        {
-            Console.WriteLine("Thread Start!");
-
-            while (_stop == false)
-            {
-                // 누군가 stop신호를 해주기를 기다린다
-            }
-
-            Console.WriteLine("Thread End!");
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            int[,] arr = new int[10000, 10000];
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+            }
 
-            Thread.Sleep(1000);
-
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-
-            t.Wait();
-
-            Console.WriteLine("종료 성공");
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
+            }
         }
     }
 }
