cb491a1411baf2e1b5d4c482932285c8ea08cbd5
Joohyuck <joohyuck.seo@gmail.com>
2021-06-15 04:07:59 +0900

    Mutext

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,41 +4,20 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class Lock
-    {
-        // bool <- 커널
-        //AutoResetEvent _available = new AutoResetEvent(true);         // 톨게이트, 자동 여닫이
-        ManualResetEvent _available = new ManualResetEvent(true);       // 문, 수동
-
-        public void Acquire()
-        {
-            // AutoResetEvent
-            _available.WaitOne();       // 입장 시도
-
-            // ManualResetEvent -> 밑에 코드 경우 원자성 코드가 아니므로 잘못된 결과가 나온다. 
-            //                     이 경우는 AutoRestEvent 쓰는것이 좋다
-            //_available.WaitOne();       // 입장 시도
-            //_available.Reset();         // 문을 닫는다
-        }
-
-        public void Release()
-        {
-            _available.Set();           // flag = true
-        }
-    }
-
     class Program
     {
         static int _num = 0;
-        static Lock _lock = new Lock();
+
+        // int ThreadId <- AutoResetEvent는 bool형인 것에 비해 Mutext는 정보를 많이 갖고 있으며 그만큼 부담이 큼
+        static Mutex _lock = new Mutex();
 
         static void Thread_1()
         {
             for (int i = 0; i < 10000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
@@ -46,9 +25,9 @@ namespace ServerCore
         {
             for (int i = 0; i < 10000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
