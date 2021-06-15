65b011477c997d6cb33177c29d2877fc8419ed58
Joohyuck <joohyuck.seo@gmail.com>
2021-06-15 16:22:56 +0900

    ReaderWriterLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,41 +6,37 @@ namespace ServerCore
 {
     class Program
     {
-        static int _num = 0;
-
-        // int ThreadId <- AutoResetEvent는 bool형인 것에 비해 Mutext는 정보를 많이 갖고 있으며 그만큼 부담이 큼
-        static Mutex _lock = new Mutex();
-
-        static void Thread_1()
-        {
-            for (int i = 0; i < 10000; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < 10000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
-        }
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            Task t1 = new Task(delegate()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
+
             t1.Start();
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(_num);
+            Console.WriteLine(count);
         }
     }
 }
